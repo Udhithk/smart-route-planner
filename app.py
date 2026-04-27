cb48661ed8dea4e379d4917c0e57d3568fc67e9f
Udhith K <udhithuk1@gmail.com>
2026-04-27 20:11:09 +0530

    enabled sidebar by defualt

--- app.py
+++ app.py
@@ -1,16 +1,9 @@
 import streamlit as st
 
-st.set_page_config(page_title="Smart Route Planner", layout="wide")
+st.set_page_config(
+    page_title="Smart Route Planner",
+    layout="wide",
+    initial_sidebar_state="expanded"   # 👈 ADD THIS
+)
 
 st.title("🌍 Smart Route Planner")
-
-st.markdown("""
-### Welcome 👋  
-
-This project includes:
-
-- 📊 **Basic Graph Planner** (Dijkstra & A*)
-- 🗺️ **Real Map Planner** (OSM-based routing)
-
-👉 Use the sidebar to navigate
-""")
